0df310843e628d2acbdb894cbbc6754b6d5e8570
haidour <ga_haidour@esi.dz>
2021-02-11 00:24:27 +0100

    Sign up backend fixed

--- lib/views/ChatRoomScreen.dart
+++ lib/views/ChatRoomScreen.dart
@@ -1,12 +1,34 @@
+import 'package:chat_app/services/Auth.dart';
+import 'package:chat_app/views/signin.dart';
 import 'package:flutter/material.dart';
 class ChatRoom extends StatefulWidget {
   @override
   _ChatRoomState createState() => _ChatRoomState();
 }
 
-class _ChatRoomState extends State<ChatRoom> {
+class _ChatRoomState extends State<ChatRoom>{
+ AuthMethod authMethod = new AuthMethod();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+actions: [
+  GestureDetector(
+    onTap: (){
+      authMethod.signOut();
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => SignIn()));
+    },
+    child: Container(
+    
+        padding: const EdgeInsets.all(8.0),
+        child: Icon(Icons.exit_to_app),
+      
+    ),
+  )
+
+],      ),
+
+    );
   }
 }
